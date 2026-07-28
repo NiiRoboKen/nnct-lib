8877fce6bc7b780037e0e4a500561a794f331d83
zen4215 <44k1.373@gmail.com>
2026-07-28 07:58:52 +0000

    refactor: add initial data

--- include/nnct/mocks/can_sender.hpp
+++ include/nnct/mocks/can_sender.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "nnct/types/can.hpp"
 #include <nnct/interfaces/can_sender.hpp>
 
 namespace nnct::mocks {
@@ -9,12 +8,12 @@ class CanSender : public interfaces::CanSender {
     public:
         void send(const types::can::Frame& frame) override;
         
-        bool called { false };
-        bool latest_is_std;
-        bool latest_is_ext;
-        uint32_t latest_id;
-        types::can::Data latest_data;
-        uint8_t latest_dlc;
+        bool called                     { false };
+        bool latest_is_std              { false };
+        bool latest_is_ext              { false };
+        uint32_t latest_id              { 0 };
+        types::can::Data latest_data    { constants::can::EMPTY };
+        uint8_t latest_dlc              { 0 };
 };
     
 }
