efc845d895fc030eb02322aac30bfe7bbe33f4a8
zen4215 <44k1.373@gmail.com>
2026-07-28 08:01:05 +0000

    style: format

--- src/nnct/mocks/can_sender.cpp
+++ src/nnct/mocks/can_sender.cpp
@@ -4,21 +4,21 @@
 namespace nnct::mocks {
     
 void CanSender::send(const types::can::Frame& frame) {
-    called = true;
-    latest_is_std = not frame.extd;
-    latest_is_ext = frame.extd;
-    latest_id = frame.id;
-    latest_data = frame.data;
-    latest_dlc = frame.dlc;
+    called          = true;
+    latest_is_std   = not frame.extd;
+    latest_is_ext   = frame.extd;
+    latest_id       = frame.id;
+    latest_data     = frame.data;
+    latest_dlc      = frame.dlc;
 }
 
 void CanSender::clear() {
-    called = false;
-    latest_is_std = false;
-    latest_is_ext = false;
-    latest_id = 0;
-    latest_data = constants::can::EMPTY;
-    latest_dlc = 0;
+    called          = false;
+    latest_is_std   = false;
+    latest_is_ext   = false;
+    latest_id       = 0;
+    latest_data     = constants::can::EMPTY;
+    latest_dlc      = 0;
 }
     
 }
