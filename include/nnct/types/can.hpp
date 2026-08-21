a3a0a6c4924d45a6708107b8fdca3263ca359da8
zen4215 <44k1.373@gmail.com>
2026-08-21 10:12:53 +0000

    style: rename dlc -> len

--- include/nnct/types/can.hpp
+++ include/nnct/types/can.hpp
@@ -10,13 +10,13 @@ using Data = std::array<uint8_t, 8>;
 struct Frame {
     uint32_t    id;
     Data        data;
-    uint8_t     dlc;
+    uint8_t     len;
     bool        extd;
 };
 
 struct FilterdFrame {
     Data    data;
-    uint8_t dlc;
+    uint8_t len;
 
     static FilterdFrame fromFrame(const Frame& frame) { return {frame.data, frame.dlc}; }
 };
