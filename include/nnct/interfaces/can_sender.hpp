8ddc7b26e9625395c53d43acee8f90b69d1e6e5b
zen4215 <44k1.373@gmail.com>
2026-08-21 10:37:50 +0000

    fix: cansender interface

--- include/nnct/interfaces/can_sender.hpp
+++ include/nnct/interfaces/can_sender.hpp
@@ -10,11 +10,11 @@ class CanSender {
     public:
         virtual void send(const types::can::Frame& frame) = 0;
 
-        void sendStandard(uint16_t id, const types::can::Data& data, uint8_t dlc) { send({id, data, dlc, false}); }
-        void sendExtended(uint32_t id, const types::can::Data& data, uint8_t dlc) { send({id, data, dlc, true}); }
+        void sendStandard(types::can::std_id_t id, const types::can::data_t& data, uint8_t len) { send({id, data, len, false}); }
+        void sendExtended(types::can::std_id_t id, const types::can::data_t& data, uint8_t len) { send({id, data, len, true}); }
 
-        void sendStandard(uint16_t id) { sendStandard(id, constants::can::EMPTY, 0); }
-        void sendExtended(uint32_t id) { sendExtended(id, constants::can::EMPTY, 0); }
+        void sendStandard(types::can::std_id_t id) { sendStandard(id, constants::can::EMPTY, 0); }
+        void sendExtended(types::can::ext_id_t id) { sendExtended(id, constants::can::EMPTY, 0); }
 };
 
 }
